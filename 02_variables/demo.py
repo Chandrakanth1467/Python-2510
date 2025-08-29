a22ae9292b0049ef234f3638c9121a1fe8ce176a
chandrakanth1467 <chandrakanth1467@gmail.com>
2025-08-29 20:11:27 +0530

    variables

---
+++ 02_variables/demo.py
@@ -0,0 +1,13 @@
+student_age=20
+student_name="yamini"
+
+print(student_age)
+print(student_name)
+
+
+print(id(student_age))
+print(id(student_name))
+
+
+print(type(student_age))
+print(type(student_name))
